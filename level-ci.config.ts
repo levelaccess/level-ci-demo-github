b4127f5fd761bf5e7871bac42d998a5caea2b07c
Yelyzaveta Gorbunova <yelyzaveta.gorbunova@levelaccess.com>
2025-12-03 17:31:36 +0200

    rename slug

--- level-ci.config.ts
+++ level-ci.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "@level-ci/cli";
 
 export default {
-  organization: "level-access-5628557662360117-rjtxk",
+  organization: "level-access-5628557662360117-org-rjtxk",
   project: "level-ci-demo-github",
   token: process.env.LEVEL_CI_TOKEN,
   reportPaths: ["./level-ci-reports"],
